527042ce20dc328ab44c28b0ae96b7e8ae84ea7a
adillari1493 <adil.lari1493@gmail.com>
2025-06-02 13:12:00 +0530

    test classes and regression suite updated

--- src/test/java/com/qa/api/gorestApi/tests/GetAUserWithDeserializationTest.java
+++ src/test/java/com/qa/api/gorestApi/tests/GetAUserWithDeserializationTest.java
@@ -10,26 +10,61 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+/**
+ * Test class for creating and retrieving a user using deserialization.
+ * Contains methods to validate user creation and retrieval via API calls.
+ */
 public class GetAUserWithDeserializationTest extends BaseTest {
 
+    /**
+     * Stores the ID of the created user for further validation.
+     */
     protected String userId;
 
+    /**
+     * Test to create a user and validate its retrieval using deserialization.
+     * Steps:
+     * 1. Create a user using a POST request.
+     * 2. Validate the response status code and extract the user ID.
+     * 3. Retrieve the user using a GET request.
+     * 4. Validate the response status code and deserialize the response body.
+     * 5. Validate the deserialized user details.
+     */
     @Test
     public void createAUserTest() {
-        User user = User.builder().name("Adil").email(StringUtils.generateRandomEmailId()).gender("male").status("active").build();
+        // Create a User object with test data
+        User user = User.builder()
+                .name("Adil")
+                .email(StringUtils.generateRandomEmailId())
+                .gender("male")
+                .status("active")
+                .build();
+
+        // Send a POST request to create the user
         Response postResponse = restClient.post(GORESTAPI_BASEURL, GORESTAPI_ENDPOINT_USERS,
                 user, null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
+
+        // Extract the user ID from the response
         userId = postResponse.jsonPath().getString("id");
+
+        // Validate the response status code is 201 (Created)
         Assert.assertEquals(postResponse.statusCode(), 201, "Validating the status code is 200");
 
+        // Send a GET request to retrieve the created user
         Response getResponse = restClient.get(GORESTAPI_BASEURL, GORESTAPI_ENDPOINT_USERS + "/" + userId,
                 null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
+
+        // Validate the response status code is 200 (OK)
         Assert.assertEquals(getResponse.statusCode(), 200, "Validating status code is 200");
+
+        // Deserialize the response body into a User object
         User userResponseBody = JsonUtils.deserialize(getResponse, User.class);
+
+        // Validate the deserialized user details
         Assert.assertNotNull(userResponseBody.getId(), "Validating that ID of the user is not NULL");
         Assert.assertEquals(userResponseBody.getName(), user.getName(), "Validating the Name of User");
         Assert.assertEquals(userResponseBody.getStatus(), user.getStatus(), "Validating the Status of User");
     }
 
-    //TODO: Practice with Deserialization with User with Array (for multiple users)
+    // TODO: Practice with Deserialization with User with Array (for multiple users)
 }
--- src/test/java/com/qa/api/productsApi/tests/ProductAPITest.java
+++ src/test/java/com/qa/api/productsApi/tests/ProductAPITest.java
@@ -9,27 +9,44 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+/**
+ * ProductAPITest class contains test cases for interacting with the Product API.
+ * It extends the BaseTest class to utilize common test setup and utilities.
+ */
 public class ProductAPITest extends BaseTest {
 
+    /**
+     * Test to retrieve all products from the Product API.
+     * Steps:
+     * 1. Send a GET request to fetch all products.
+     * 2. Validate the response status code.
+     * 3. Deserialize the response body into an array of Product objects.
+     * 4. Print the details of each product to the console.
+     */
     @Test
     public void getAllProductsTest() {
+        // Send a GET request to fetch all products
         Response allProductsResponse = restClient.get(FAKESTOREAPI_BASEURL, FAKESTOREAPI_PRODUCTS_ENDPOINT,
                 null, null, AuthType.NO_AUTH, ContentType.ANY);
+
+        // Validate the response status code is 200 (OK)
         Assert.assertEquals(allProductsResponse.statusCode(), 200, "Validating status code is 200");
+
+        // Deserialize the response body into an array of Product objects
         Product[] products = JsonUtils.deserialize(allProductsResponse, Product[].class);
-        for(Product p : products)
-        {
+
+        // Iterate through the products and print their details to the console
+        for(Product p : products) {
             System.out.println("<<<<<<<<<<Product details are>>>>>>>>>>");
-            System.out.println("Product id: "+ p.getId());
-            System.out.println("Product title: "+ p.getTitle());
-            System.out.println("Product price: "+ p.getPrice());
-            System.out.println("Product description: "+ p.getDescription());
-            System.out.println("Product category: "+ p.getCategory());
-            System.out.println("Product image link: "+ p.getImage());
-            System.out.println("Product rate is: "+p.getRating().getRate());
-            System.out.println("Product count is: "+p.getRating().getCount());
+            System.out.println("Product id: " + p.getId());
+            System.out.println("Product title: " + p.getTitle());
+            System.out.println("Product price: " + p.getPrice());
+            System.out.println("Product description: " + p.getDescription());
+            System.out.println("Product category: " + p.getCategory());
+            System.out.println("Product image link: " + p.getImage());
+            System.out.println("Product rate is: " + p.getRating().getRate());
+            System.out.println("Product count is: " + p.getRating().getCount());
             System.out.println("========================================");
         }
     }
-
 }
